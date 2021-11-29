f07aecdd52b1f04319d2d95a4643d8cd94823d20
Marc Hanheide <marc@hanheide.net>
2021-11-29 19:03:39 +0000

    not only build binary

--- run.sh
+++ run.sh
@@ -12,6 +12,6 @@ cd /package/$PACKAGE_NAME
 apt-get update 
 mk-build-deps -i -r -t "apt-get -o Debug::pkgProblemResolver=yes --no-install-recommends -y"
 gbp buildpackage --git-ignore-branch --git-ignore-new -S "$sign_arg"
-dpkg-buildpackage "$sign_arg" -b
+dpkg-buildpackage "$sign_arg" 
 
 # example run: docker run -it --rm -e PACKAGE_NAME=mqtt_bridge -e GPG_KEY=920D962674D20298F82483C72B1C511EE8905F4E -v ~/.gnupg:/root/.gnupg -v `pwd`/..:/package debuild 
